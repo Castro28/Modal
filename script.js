a8e3de6b013f09b3f040f2120cbe9294ff09c864
Castro28 <waltercstr28@gmail.com>
2023-10-12 14:48:21 -0700

    finished with opening and closing modals

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnsOpenModal[i].addEventListener('click', openModal);
 
 const closeModal = function () {
   modal.classList.add('hidden');
@@ -19,8 +20,4 @@ const closeModal = function () {
 };
 
 btnCloseModal.addEventListener('click', closeModal);
-
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+overlay.addEventListener('click', closeModal);
